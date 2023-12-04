935b56b271366fa3cfbd2371ca932bdead077b91
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-12-04 03:33:10 +0000

    Add solution for day 2, part 2

---
+++ day_02_2.py
@@ -0,0 +1,36 @@
+"""Part 2 is very simple. We just need to find the maximum number of cubes foreach round
+and multiply them together. Then add them all up.
+Since we already have the Game class, we can just use that.
+"""
+
+from day_02_1 import Game
+from utils import read_input
+
+
+def solution(file_path: str):
+    """Solution for day 2, part 2
+    """
+    total: int = 0
+
+    data = read_input(file_path)
+    games = [Game(d) for d in data]
+
+    for game in games:
+        red: int = 0
+        green: int = 0
+        blue: int = 0
+        for round_ in game.rounds:
+            if round_.red > red:
+                red = round_.red
+            if round_.green > green:
+                green = round_.green
+            if round_.blue > blue:
+                blue = round_.blue
+        power = red * green * blue
+        total += power
+
+    return total
+
+
+if __name__ == "__main__":
+    print(solution("inputs/day_02_2.txt"))
--- tests/test_day_02.py
+++ tests/test_day_02.py
@@ -1,6 +1,12 @@
 from day_02_1 import solution as part1
+from day_02_2 import solution as part2
 
 
 def test_part1():
     """Test part 1"""
     assert part1("tests/test_day_02_1.txt", 12, 13, 14) == 8
+
+
+def test_part2():
+    """Test part 2"""
+    assert part2("tests/test_day_02_2.txt") == 2286
