dfed0eac7e965510db1187df23f5cbe7530a8b98
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-12-04 18:34:07 +0000

    Add solution for day 4 part 2

---
+++ day_04_2.py
@@ -0,0 +1,55 @@
+"""If there is a winning number on a card, the next card(s) is duplicated and added to the end of the list.
+Then, count the number of cards in the list.
+"""
+from utils import read_input
+import re
+
+def parse_data(data: str) -> tuple[int, int]:
+    """Parse data into Card
+    Args:
+        data (str): The data to parse
+    Returns:
+        tuple[int, int]: The card number and number of wins
+    """
+    card_num = data \
+            .split(":")[0]
+    
+    card_num = re.search(r"\d+", card_num)[0]
+    card_num = int(card_num)
+
+    winning_nums = data \
+            .split(":")[1] \
+            .split("|")[0] \
+            .strip() \
+            .split(" ")
+    winning_nums = [num.strip() for num in winning_nums if num.strip() != ""]
+
+    hands = data \
+            .split(":")[1] \
+            .split("|")[1] \
+            .strip() \
+            .split(" ")
+    hands = [hand.strip() for hand in hands if hand.strip() != ""]
+    num_of_wins = len(set(hands) & set(winning_nums))
+    
+    return card_num, num_of_wins
+
+
+def solution(file_path: str) -> int:
+    """Solution to day 4 part 2.
+    """
+    data = read_input(file_path)
+    for line in data:
+        card_num, num_of_wins = parse_data(line)  # this line could be better optimized
+        if num_of_wins > 0:
+            for i in range(num_of_wins):
+                try:
+                    data.append(data[card_num + i])
+                except IndexError:
+                    pass
+    
+    return len(data)
+
+
+if __name__ == "__main__":
+    print(solution("inputs/day_04_2.txt"))
--- tests/test_day_04.py
+++ tests/test_day_04.py
@@ -1,11 +1,12 @@
 from day_04_1 import solution as part1
-# from day_04_2 import solution as part2
+from day_04_2 import solution as part2
 
 
 def test_part1():
     """Test part 1"""
     assert part1("tests/test_day_04_1.txt") == 13
 
-# def test_part2():
-#     """Test part 2"""
-#     assert part2("tests/test_day_03_2.txt") == 467835
+
+def test_part2():
+    """Test part 2"""
+    assert part2("tests/test_day_04_2.txt") == 30
